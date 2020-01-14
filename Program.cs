3e4e112aed659b48afa918fabc4b2ba21712ae51
Jucilaine <juci.antunes@gmail.com>
2020-01-14 16:29:37 -0300

    Acrescentado msg tenha um bom dia

--- Program.cs
+++ Program.cs
@@ -7,8 +7,10 @@ namespace projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia");
+        
             Console.WriteLine("Como vc esta hoje?");
+
+            Console.WriteLine("Tenha um bom dia");
         }
     }
 }
